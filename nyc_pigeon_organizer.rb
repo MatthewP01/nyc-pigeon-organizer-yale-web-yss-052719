58fff34524f6016b894aa5ef5061098ee329dc67
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 19:45:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |attribute, info|
+    binding.pry
+    info.each do |key, name|
+
+    end
+  end
+
+
 end
