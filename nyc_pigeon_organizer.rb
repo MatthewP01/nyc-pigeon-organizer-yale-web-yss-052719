9ac704e353fe6d2d33096dc298b3d83b084b6b2e
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 20:16:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -32,7 +32,7 @@ def nyc_pigeon_organizer(data)
     end
   end
 
-  data[:gender].each do |gen_key, gen_value|
+  data[:lives].each do |gen_key, gen_value|
     gen_value.each do |name|
       hash_key.each do |gend|
         if name == gend
